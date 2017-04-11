73b17a8c1f0f65acb2d70666fdbd5aeb7301e339
Jan-Sebastian Fabík <honza@fabik.org>
2017-04-11 11:20:14 +0200

    Clean up javascript

--- www/js/main.js
+++ www/js/main.js
@@ -1,17 +1,3 @@
 jQuery(function($) {
 	$.nette.init();
-
-	var $posts = $('.ut-posts');
-
-	if ($posts.length) {
-		var conn = new WebSocket('ws://localhost:3001/broadcast');
-
-		conn.onmessage = function (e) {
-			if (e.data === 'refresh') {
-				$.nette.ajax({
-					url: $posts.data('refresh-url')
-				});
-			}
-		};
-	}
 });
