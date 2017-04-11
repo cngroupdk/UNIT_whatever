8ac872158f733051e8437e0a0718f41f85869a14
Jan-Sebastian Fabík <honza@fabik.org>
2017-04-11 10:19:03 +0200

    Added description attribute

--- app/model/Poll.php
+++ app/model/Poll.php
@@ -9,6 +9,7 @@ use Nextras\Orm\Relationships\OneHasMany;
  * @property int $id {primary}
  * @property User $user {m:1 User::$polls}
  * @property string $name
+ * @property string $description
  * @property OneHasMany|Category[] $categories {1:m Category::$poll}
  * @property OneHasMany|Feedback[] $feedbacks {1:m Feedback::$poll}
  */
---
+++ migrations/structures/2017-04-11-101842-description.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `polls`
+ADD `description` longtext COLLATE 'utf8mb4_czech_ci' NOT NULL;
