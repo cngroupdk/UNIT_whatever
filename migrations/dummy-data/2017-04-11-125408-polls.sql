5d5b527190a2cab345a2df437bdf9113028401df
Richard Trebichavský <richard.trebichavsky@gmail.com>
2017-04-11 10:33:53 +0200

    Dummy data: 1 poll with 3 categories

---
+++ migrations/dummy-data/2017-04-11-125408-polls.sql
@@ -0,0 +1,4 @@
+INSERT INTO `polls` (`user_id`, `name`, `description`) VALUES ('1', 'Unit 2017', 'Feedback to Unit 2017');
+INSERT INTO `categories` (`poll_id`, `name`) VALUES ('1', 'Food');
+INSERT INTO `categories` (`poll_id`, `name`) VALUES ('1', 'Organization');
+INSERT INTO `categories` (`poll_id`, `name`) VALUES ('1', 'Coffee');
